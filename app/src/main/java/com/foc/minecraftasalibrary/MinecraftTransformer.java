bfc6d9252eec125ada8e7dda18d5f24cc11adf48
Technisha <proxneon960@gmail.com>
2022-09-11 21:57:57 +0100

    Produce two jars because of how mappings fuck up everything lmao

--- app/src/main/java/com/foc/minecraftasalibrary/MinecraftTransformer.java
+++ app/src/main/java/com/foc/minecraftasalibrary/MinecraftTransformer.java
@@ -15,6 +15,7 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.*;
 
 public class MinecraftTransformer {
@@ -44,61 +45,39 @@ public class MinecraftTransformer {
         Utils.downloadToFile(mappingsUrl, mappingsFile.toString());
     }
 
-    public static void flattenMCJar(final Path officialJar, final Path outputJar) throws IOException {
+    public static void flattenAndRemapJar(final Path officialJar, final Path mappings, final Path depsJar, final Path remappedJar) throws IOException {
         Path bundleDir = Path.of(".temp/minecraftBundle").toAbsolutePath();
 
         new ZipFile(officialJar.toAbsolutePath().toString()).extractAll(bundleDir.toString());
 
+        final String mcVersion = new JSONObject(Files.readString(Path.of(bundleDir.toString(), "version.json"))).getString("id");
+
         // Get all jars within the bundled jar
         String[] jars = Files.readString(Path.of(bundleDir.toString(), "META-INF/classpath-joined")).split(";");
+
+        Path serverJar = Path.of(bundleDir.toString(), "META-INF/versions", mcVersion, "server-"+mcVersion+".jar").toAbsolutePath();
+
+        new ZipFile(serverJar.toString()).extractAll(".temp/minecraftServerJar");
+
+        Utils.remapMinecraftServerJar(mappings, serverJar, remappedJar);
 
         for (String jar : jars) {
             Path path = Path.of(bundleDir.toString(), "META-INF", jar).toAbsolutePath();
 
-            new ZipFile(path.toString()).extractAll(".temp/outputtedjars");
+            if (!path.equals(serverJar)) {
+                new ZipFile(path.toString()).extractAll(".temp/outputtedjars");
+            } else {
+                continue;
+            }
         }
 
-
-        // NOTE: This jar isn't runnable, it's only made so it can be loaded by a classloader.
-        new ZipFile(outputJar.toString()).addFiles(Arrays.asList(Objects.requireNonNull(new File(".temp/outputtedjars").listFiles())));
+        // NOTE: This jar isn't runnable, it's sole purpose is to make it loadable by a classloader.
+        new ZipFile(depsJar.toString()).addFiles(Arrays.asList(Objects.requireNonNull(new File(".temp/outputtedjars").listFiles())));
     }
 
-    public static void remapMinecraftJar(final Path mappings, final Path unpackedJar, final Path outputJar) {
-        Path[] classpath = new Path[0];
-
-        TinyRemapper remapper = TinyRemapper.newRemapper()
-                .withMappings(TinyUtils.createTinyMappingProvider(mappings, "official", "named")) // Just saying it's from official names to mapping names
-                .build();
-
-        try (OutputConsumerPath outputConsumer =
-                     new OutputConsumerPath.Builder(outputJar).build()) {
-            outputConsumer.addNonClassFiles(unpackedJar, NonClassCopyMode.FIX_META_INF, remapper);
-
-            remapper.readInputs(unpackedJar);
-            remapper.readClassPath(classpath);
-
-            remapper.apply(outputConsumer);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            remapper.finish();
-        }
-    }
-
-    public static URLClassLoader createClassLoader(final Path remappedJar) throws MalformedURLException {
+    public static URLClassLoader createClassLoader(URL... otherUrls) throws MalformedURLException {
         ArrayList<URL> urls = new ArrayList<>();
 
-        urls.add(remappedJar.toAbsolutePath().toUri().toURL());
-
-        URL[] urlArray = urls.toArray(new URL[0]);
-
-        return new URLClassLoader(urlArray);
-    }
-
-    public static URLClassLoader createClassLoader(final Path remappedJar, URL... otherUrls) throws MalformedURLException {
-        ArrayList<URL> urls = new ArrayList<>();
-
-        urls.add(remappedJar.toAbsolutePath().toUri().toURL());
         urls.addAll(List.of(otherUrls));
 
         URL[] urlArray = urls.toArray(new URL[0]);
@@ -107,20 +86,10 @@ public class MinecraftTransformer {
     }
 
     // Allows you to use a custom parent classloader
-    public static URLClassLoader createClassLoader(final Path remappedJar, ClassLoader parentClassloader) throws MalformedURLException {
+
+    public static URLClassLoader createClassLoader(ClassLoader parentClassloader, URL... otherUrls) throws MalformedURLException {
         ArrayList<URL> urls = new ArrayList<URL>();
 
-        urls.add(remappedJar.toAbsolutePath().toUri().toURL());
-
-        URL[] urlArray = urls.toArray(new URL[0]);
-
-        return new URLClassLoader(urlArray);
-    }
-
-    public static URLClassLoader createClassLoader(final Path remappedJar, ClassLoader parentClassloader, URL... otherUrls) throws MalformedURLException {
-        ArrayList<URL> urls = new ArrayList<URL>();
-
-        urls.add(remappedJar.toAbsolutePath().toUri().toURL());
         urls.addAll(List.of(otherUrls));
 
         URL[] urlArray = urls.toArray(new URL[0]);
--- app/src/main/java/com/foc/minecraftasalibrary/Utils.java
+++ app/src/main/java/com/foc/minecraftasalibrary/Utils.java
@@ -1,5 +1,10 @@
 package com.foc.minecraftasalibrary;
 
+import net.fabricmc.tinyremapper.NonClassCopyMode;
+import net.fabricmc.tinyremapper.OutputConsumerPath;
+import net.fabricmc.tinyremapper.TinyRemapper;
+import net.fabricmc.tinyremapper.TinyUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -11,6 +16,28 @@ import java.nio.file.StandardCopyOption;
 import java.util.Scanner;
 
 public class Utils {
+    public static void remapMinecraftServerJar(final Path mappings, final Path serverJar, final Path outputJar) {
+        Path[] classpath = new Path[0];
+
+        TinyRemapper remapper = TinyRemapper.newRemapper()
+                .withMappings(TinyUtils.createTinyMappingProvider(mappings, "official", "named")) // Just saying it's from official names to mapping names
+                .build();
+
+        try (OutputConsumerPath outputConsumer =
+                     new OutputConsumerPath.Builder(outputJar).build()) {
+            outputConsumer.addNonClassFiles(serverJar, NonClassCopyMode.FIX_META_INF, remapper);
+
+            remapper.readInputs(serverJar);
+            remapper.readClassPath(classpath);
+
+            remapper.apply(outputConsumer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            remapper.finish();
+        }
+    }
+
     public static String readStringFromURL(String requestURL) throws IOException {
         try (Scanner scanner = new Scanner(new URL(requestURL).openStream(),
                 StandardCharsets.UTF_8)) {
